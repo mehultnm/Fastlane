027a528f2fd623108de208b5318a63f275234e2c
mehultnm <Heet@phoenixinnovate.co.uk>
2023-07-12 18:25:29 +0530

    push

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import {View, Text,} from 'react-native'
 const App =() => {
     return(
         <View style={{display:'flex',backgroundColor:'#f4f4f4',alignItems:"center",justifyContent:"center",height:'100%'}}>
-            <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>FASTLANE</Text>
+            <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>FASTLANE FIREBASE</Text>
+            <Text style={{color:'#000',fontSize:25,fontWeight:'700'}}>FIREBASE</Text>
         </View>
     )
 }
